612f30269ae30c94325016c42a50c214d0532acb
Henryck <ryck302@gmail.com>
2022-11-19 23:28:15 -0300

    requisito 09

---
+++ src/Monster.ts
@@ -0,0 +1,33 @@
+import { SimpleFighter } from './Fighter';
+
+class Monters implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    this._lifePoints = this.lifePoints - attackPoints > 0 
+      ? this.lifePoints - attackPoints
+      : -1;
+  
+    return this._lifePoints;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this.strength);
+  }
+}
+
+export default Monters;
