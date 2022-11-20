21e8d2712177b44519461c5bf7d744a48c96cb18
Henryck <ryck302@gmail.com>
2022-11-19 23:49:30 -0300

    requisito 10

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,20 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+class PVP extends Battle {
+  constructor(private player1: Fighter, private player2: Fighter) {
+    super(player1);
+  }
+
+  fight(): number {
+    while (this.player1.lifePoints > 0 && this.player2.lifePoints > 0) {
+      this.player1.attack(this.player2);
+      if (this.player2.lifePoints > 0) {
+        this.player2.attack(this.player1);
+      }
+    }
+    return this.player.lifePoints === -1 ? -1 : 1;
+  }
+}
+
+export default PVP;
